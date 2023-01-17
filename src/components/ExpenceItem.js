1325c6c88ad2698b5f80663d30f9a88ae3a532d3
setx <setx-99@tut.by>
2023-01-17 19:41:39 +0300

    task_3

---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,14 @@
+function ExpenceItem() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>pirororooror</h2>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenceItem;
